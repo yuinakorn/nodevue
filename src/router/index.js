91e62fadbede7b6e4d71d89639ab57a905040467
yuinakorn <yuinakorn@hotmail.com>
2022-07-08 22:02:57 +0700

    uf 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from "@/views/Login";
+import PageNotFound from "@/views/PageNotFound";
 
 const routes = [
     {
@@ -10,7 +12,7 @@ const routes = [
     {
         path: '/login',
         name: 'Login',
-        component: () => import('../views/Login.vue')
+        component: Login
     },
     {
         path: '/about',
@@ -19,7 +21,13 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'not-found',
+        component: PageNotFound
+    },
+
 ]
 
 const router = createRouter({
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,85 @@
+<template>
+  <!--  set center -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <section class="page_404">
+          <div class="four_zero_four_bg">
+            <h1 class="text-center ">404</h1>
+          </div>
+
+          <div class="contant_box_404">
+            <h3 class="h2">
+              Look like you're lost
+            </h3>
+            <p>the page you are looking for not available!</p>
+            <button class="link_404 btn rounded-pill" onclick="history.back()">Go back</button>
+          </div>
+        </section>
+
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: "PageNotFound",
+
+}
+</script>
+
+<style scoped>
+
+/*======================
+    404 page
+=======================*/
+
+
+.page_404 {
+  padding: 40px 0;
+  background: #fff;
+  font-family: 'Arvo', serif;
+  width: 700px;
+  margin: 0 auto;
+}
+
+.page_404 img {
+  width: 100%;
+}
+
+.four_zero_four_bg {
+
+  background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+  height: 400px;
+  background-position: center;
+  width: 700px;
+
+}
+
+
+.four_zero_four_bg h1 {
+  font-size: 80px;
+}
+
+.four_zero_four_bg h3 {
+  font-size: 80px;
+}
+
+.link_404 {
+  color: #fff !important;
+  padding: 10px 20px;
+  /*background: #39ac31;*/
+  background: #08a188;
+  margin: 20px 0;
+  display: inline-block;
+}
+
+.contant_box_404 {
+  margin: -50px auto 0 auto;
+  text-align: center;
+}
+
+</style>
